07478ffc63dc61e3c52e161b0dffb6efb58ada79
Lubna <lubnafirdaus2309@gmail.com>
2025-05-21 14:08:14 -0700

    angle function final

--- math.py
+++ math.py
@@ -1,4 +1,7 @@
 import numpy as np 
 
 def get_angle(a, b, c):
-    
+    #direction of vectors
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0]) #subracting em to get angle b/w em
+    angle = np.abs(np.degrees(radians))
+    return angle
